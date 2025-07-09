7fc3b216890842a1f125e0a8e513736ff8ebc457
Prasoon Kumar <prasoonkumar467@gmail.com>
2025-07-09 18:42:42 +0530

    Add theme toggle functionality and example card component

---
+++ app/components/ExampleCard.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+
+export default function ExampleCard() {
+  return (
+    <div className="mt-6 flex items-center justify-center p-2 sm:p-4">
+      <div className="
+        w-full max-w-md sm:max-w-xl 
+        px-4 sm:px-6 py-6 sm:py-10 
+        rounded-2xl shadow-2xl
+        bg-white/80 dark:bg-black/50 backdrop-blur-lg
+        border border-gray-200/50 dark:border-white/10
+      ">
+        MY PORTFOLIO
+
+      </div>
+    </div>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "next-themes";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,11 +8,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={` antialiased`}>
+        <ThemeProvider
+          attribute="data-theme"
+          defaultTheme="system"
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
